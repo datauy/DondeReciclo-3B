25330a2a87c4e7c63fccb8f8215c7829a21d6011
Fernando Uval <fernandouval@gmail.com>
2020-07-15 01:59:33 -0300

    Se agrega manejo de excepciones al envío de mails y respuestas en json

--- app/controllers/utils_controller.rb
+++ app/controllers/utils_controller.rb
@@ -1,14 +1,25 @@
 class UtilsController < ApplicationController
   #
   def contact_email
-    AdminMailer.
-      with(
-        name: params[:name],
-        email: params[:email],
-        body: params[:body],
-        subject: params[:subject]
-      ).
-      contact.
-      deliver
+    begin
+      AdminMailer.
+        with(
+          name: params[:name],
+          email: params[:email],
+          body: params[:body],
+          subject: params[:subject]
+        ).
+        contact.
+        deliver
+      render json: {
+        error:0,
+        message:"delivered sccessfully"
+      }, status: :ok
+    rescue StandardError => e
+      render json: {
+        error: 1,
+        message: e.to_s
+      }, status: 500
+    end
   end
 end
