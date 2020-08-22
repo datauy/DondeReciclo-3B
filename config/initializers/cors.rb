899a7a6bc843a0172c7aa45b78e9c15e6d50a2dc
Fernando Uval <fernandouval@gmail.com>
2020-08-22 03:00:36 -0300

    Se agrega CORS para manejo de passwords

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -24,4 +24,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         headers: ['Access-Control-Allow-Headers','Content-Type','X-Amz-Date','Authorization','X-Api-Key','Origin','Accept','Access-Control-Allow-Headers','Access-Control-Allow-Methods','Access-Control-Allow-Origin'],
         methods: [:post]
     end
+    allow do
+      origins 'https://dr.dev.data.org.uy', 'http://localhost', 'http://localhost:8100', 'localhost', 'https://dondereciclo.uy', 'https://dondereciclo.com.uy', 'https://www.dondereciclo.uy', 'https://www.dondereciclo.com.uy'
+      resource '/password/*',
+        headers: ['Access-Control-Allow-Headers','Content-Type','X-Amz-Date','Authorization','X-Api-Key','Origin','Accept','Access-Control-Allow-Headers','Access-Control-Allow-Methods','Access-Control-Allow-Origin'],
+        methods: [:post]
+    end
 end
