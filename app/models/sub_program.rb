8b8303ef1d94b65d18ec3eead8a9cb97b84f7b7d
Fernando Uval <fernandouval@gmail.com>
2021-12-03 18:25:53 -0300

    Se agregan funciones de importación y se cargan nuevos datos de Colombia

--- app/models/sub_program.rb
+++ app/models/sub_program.rb
@@ -15,4 +15,49 @@ class SubProgram < ApplicationRecord
   class HABTM_Materials
     self.primary_key = :material_id
   end
+  #
+  def add_wastes_or_materials(wastes_names, update)
+    errors = self.add_wastes(wastes_names, update)
+    errors += self.add_materials(wastes_names, update)
+  end
+  #
+  def add_wastes(wastes_names, update)
+    log = []
+    wastes_names.each do |waste_name|
+      waste = Waste.search_name(waste_name).first
+      if waste.present?
+        if !self.wastes.include? waste
+          self.wastes << waste
+          log << "Residuo AGREGADO: #{waste_name}"
+        end
+      else
+        log << "Residuo NO ENCONTRADO: #{waste_name}"
+      end
+    end
+    if update
+      self.save
+    end
+    return log
+  end
+  #
+  def add_materials(wastes_names, update)
+    log = []
+    wastes_names.each do |waste_name|
+      material = Material.search_name(waste_name).first
+      if material.present?
+        if !self.materials.include? material
+          self.materials << material
+          log << "Material AGREGADO: #{waste_name}"
+        end
+      else
+        log << "Material NO ENCONTRADO: #{waste_name}"
+      end
+    end
+    #Add only new wastes
+    if update
+      self.save
+    end
+    return log
+  end
+
 end
