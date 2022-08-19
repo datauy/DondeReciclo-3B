8ac31d8c2cb9f7f673d900d168303f3fb5552dbb
Fernando Uval <fernandouval@gmail.com>
2022-08-19 14:19:05 -0300

    Se agregan dimensiones a búsquedas predefinidas y detalles a Países. Se ajustan modelos y admin.

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,8 @@
 class Country < ApplicationRecord
   has_many :users
-  
+
   has_many :dimension_relations
   has_many :dimensions, through: :dimension_relations
+
+  has_many :predefined_searches
 end
--- app/models/predefined_search.rb
+++ app/models/predefined_search.rb
@@ -1,5 +1,6 @@
 class PredefinedSearch < ApplicationRecord
   belongs_to :country
+  belongs_to :dimension
 
   has_many :materials_relations
   has_many :materials, through: :materials_relations
