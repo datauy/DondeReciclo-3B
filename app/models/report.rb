6957455d0e15243da43c22f7f4c612814c1a00d0
Fernando Uval <fernandouval@gmail.com>
2023-02-15 14:57:32 -0300

    Se agrega función de borrado de usuarios

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,7 @@
 class Report < ApplicationRecord
   belongs_to :sub_program, optional: true
   belongs_to :country
+  belongs_to :user
 
   has_many :materials_relations
   has_many :materials, through: :materials_relations
